f9a79f5580bd86b94dea5c93dd0c50d5c903fd12
cairomedeiros <cairomedeiros13@gmail.com>
2023-06-17 13:43:03 -0300

    criando uma interface simples e utilizando o LazyColumn

--- app/src/main/java/com/example/controlefinanceiro/MainActivity.kt
+++ app/src/main/java/com/example/controlefinanceiro/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.controlefinanceiro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.controlefinanceiro.ui.theme.ControleFinanceiroTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +33,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ControleFinanceiroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column() {
+                    BemVindo()
+                    Transacoes()
                 }
             }
         }
@@ -30,17 +43,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ControleFinanceiroTheme {
-        Greeting("Android")
+fun Transacoes(){
+    LazyColumn(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(8.dp)
+    ){
+        items(transacoesLista.size){index ->
+            Transacao(index)
+        }
     }
 }
+
+@Composable
+private fun Transacao(index: Int) {
+    Card(
+        elevation = CardDefaults.cardElevation(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Row() {
+            Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = "teste"
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(
+                text = transacoesLista[index],
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun BemVindo(){
+    Row(modifier = Modifier.padding(16.dp)){
+        Text(
+            text = "Bem vindo de volta, \n Cairo Medeiros",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.weight(1f)
+        )
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = "Limpar Transações",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+private val transacoesLista = listOf<String>(
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira",
+    "Café", "Aluguel", "Academia", "Feira"
+)
